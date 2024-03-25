87d3dad91756b3b74ed5d6c6de0d53012780d12c
hackertronix <leavethemails@gmail.com>
2024-03-25 20:30:04 +0530

    cleanup

--- 06-material-design/Final/app/src/main/java/com/yourcompany/android/githubusers/MainActivity.kt
+++ 06-material-design/Final/app/src/main/java/com/yourcompany/android/githubusers/MainActivity.kt
@@ -35,27 +35,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import com.yourcompany.android.githubusers.components.GitHubRepoCard
 import com.yourcompany.android.githubusers.ui.theme.GithubUserAppTheme
 import com.yourcompany.android.githubusers.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -75,46 +62,7 @@ class MainActivity : ComponentActivity() {
             GithubUserAppTheme {
                 Column(modifier = Modifier.padding(16.dp)) {
                     state?.forEach { repo ->
-                        Card(shape = RoundedCornerShape(16.dp), elevation = 2.dp) {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier.padding(16.dp)
-                            ) {
-                                AsyncImage(
-                                    modifier = Modifier
-                                        .size(75.dp)
-                                        .clip(CircleShape),
-                                    model = repo.owner.avatarUrl,
-                                    contentDescription = null,
-                                )
-
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(16.dp)
-                                ) {
-                                    Text(
-                                        text = repo.name,
-                                        style = MaterialTheme.typography.h5,
-                                    )
-                                    Spacer(modifier = Modifier.height(8.dp))
-                                    repo.description?.let {
-                                        Text(
-                                            text = it,
-                                            style = MaterialTheme.typography.body1
-                                        )
-                                    }
-                                    Spacer(modifier = Modifier.height(8.dp))
-                                    Text(
-                                        text = repo.htmlUrl,
-                                        style = MaterialTheme.typography.subtitle2,
-                                        color = MaterialTheme.colors.secondary,
-                                        textDecoration = TextDecoration.Underline
-                                    )
-
-                                }
-                            }
-                        }
+                        GitHubRepoCard(repo)
                         Spacer(modifier = Modifier.height(16.dp))
                     }
                 }
