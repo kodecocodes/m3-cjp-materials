5512b37165aff7d8e20d022c75d46759625ec6b4
hackertronix <leavethemails@gmail.com>
2024-06-08 16:55:30 +0530

    update final projects

--- 05-leverage-components/Final/app/src/main/java/com/yourcompany/android/githubusers/MainActivity.kt
+++ 05-leverage-components/Final/app/src/main/java/com/yourcompany/android/githubusers/MainActivity.kt
@@ -34,28 +34,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import com.yourcompany.android.githubusers.components.GitHubRepoCard
 import com.yourcompany.android.githubusers.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -73,47 +60,7 @@ class MainActivity : ComponentActivity() {
             val state by viewModel.state.observeAsState()
             Column(modifier = Modifier.padding(16.dp)) {
                 state?.forEach { repo ->
-                    Card(shape = RoundedCornerShape(16.dp), elevation = 2.dp) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier.padding(16.dp)
-                        ) {
-                            AsyncImage(
-                                modifier = Modifier
-                                    .size(75.dp)
-                                    .clip(CircleShape),
-                                model = repo.owner.avatarUrl,
-                                contentDescription = null,
-                            )
-
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp)
-                            ) {
-                                Text(
-                                    text = repo.name,
-                                    fontSize = 16.sp,
-                                    fontWeight = FontWeight.SemiBold,
-                                )
-                                Spacer(modifier = Modifier.height(8.dp))
-                                repo.description?.let {
-                                    Text(
-                                        text = it,
-                                        fontSize = 14.sp,
-                                    )
-                                }
-                                Spacer(modifier = Modifier.height(8.dp))
-                                Text(
-                                    text = repo.htmlUrl,
-                                    fontSize = 14.sp,
-                                    fontWeight = FontWeight.Light,
-                                    textDecoration = TextDecoration.Underline
-                                )
-
-                            }
-                        }
-                    }
+                    GitHubRepoCard(repo)
                     Spacer(modifier = Modifier.height(16.dp))
                 }
             }
